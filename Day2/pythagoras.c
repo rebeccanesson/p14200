d079aa66397320a2c90a18142f7b02e53056c63a
Rebecca Nesson <rebeccanesson@gmail.com>
2018-04-26 16:40:10 -0400

    pythagoras

---
+++ Day2/pythagoras.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <cs50.h> 
+#include <math.h> 
+
+int main(void) { 
+    
+    // Ask the user for a side length
+    
+    // Store the answer in a variable
+    
+    // Ask the user for the other side length
+    
+    // Store the answer in a variable
+    
+    // Calculate the hypotenuse length using sqrt
+    
+    // Tell the user the length of the hypotenuse 
+
+}
